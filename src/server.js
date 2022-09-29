4ef1c8b7ac04ad3dcd52911cf69ad0acff8ec496
BRUNO BARBOSA <brunobdq32@gmail.com>
2022-09-29 10:05:54 -0400

    rota GET categories

---
+++ src/server.js
@@ -0,0 +1,33 @@
+import express from 'express';
+import cors from 'cors';
+import pkg from 'pg';
+
+import dotenv from 'dotenv';
+dotenv.config();
+
+const { Pool } = pkg;
+const connection = new Pool({
+    host: 'localhost',
+    port: 5432,
+    user: 'postgres',
+    password: '01234560',
+    database: 'boardcamp',
+})
+
+const server = express();
+server.use(cors());
+server.use(express.json());
+
+server.get('/categories', async(req, res) =>{
+    const categorias = await connection.query('SELECT * FROM categories;');
+    console.log(categorias);
+    res.send(categorias);
+});
+
+server.get('/status', (req, res) =>{
+    res.send('ok');
+})
+
+
+
+server.listen(4000, () => console.log('listen on port 4000'));
