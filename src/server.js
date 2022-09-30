45f670ec9d6cecbbc2b29df82fc3297557864ac6
BRUNO BARBOSA <brunobdq32@gmail.com>
2022-09-30 08:30:25 -0400

    criando item n banco com letra maiuscula

--- src/server.js
+++ src/server.js
@@ -1,42 +1,62 @@
 import express from 'express';
 import cors from 'cors';
-import pkg from 'pg';
-const { Pool } = pkg;
-
+import pg from 'pg';
+const { Pool } = pg;
 import dotenv from 'dotenv';
 dotenv.config();
 
+import categoriesRoute from './routes/categories.Route.js'
+
+
 const connection = new Pool({
-    host: 'localhost',
-    port: 5432,
-    user: 'postgres',
-    password: '01234560',
-    database: 'boardcamp',
-})
+    connectionString: process.env.DATABASE_URL,
+  });
 
 const server = express();
 server.use(cors());
 server.use(express.json());
 
-server.get('/categories', async(req, res) =>{
+server.use(categoriesRoute);
+
+/* server.get('/categories', async(req, res) =>{
     const categorias = await connection.query('SELECT * FROM categories;');
     console.log(categorias);
     res.send(categorias.rows);
-});
+}); */
 
 server.post('/categories', async (req, res) => {
     const { name } = req.body;
-    const categorias = await connection.query ("INSERT INTO categories (name) values ($1);", [name]);
-
-    console.log(categorias);
-
-    res.sendStatus(200);
+     await connection.query ("INSERT INTO categories (name) values ($1);", [name]);
+    res.sendStatus(201);
 });
 
+server.get('/games', async (req, res) => {
+    const listaGames = await connection.query('SELECT * FROM games;');
+    console.log(listaGames);
+    res.send(listaGames.rows);
+});
+
+server.post('/games', async (req, res) =>{
+    
+
+    const {
+        name,
+        image,
+        stockTotal,
+        categoryId,
+        pricePerDay
+      } = req.body;
+    await connection.query (`
+    INSERT INTO games (name, image, "stockTotal", "categoryId", "pricePerDay") VALUES ('Banco Imobiliário', 'http://', 3, 1, 1500);
+  `);
+    
+    res.sendStatus(201)
+})
 
 
 
 server.get('/status', (req, res) =>{
     res.send('ok');
 })
-server.listen(4000, () => console.log('listen on port 4000'));
+const PORT = process.env.PORT || 4000
+server.listen(PORT, () => console.log(`Listen on port ${PORT}`));
